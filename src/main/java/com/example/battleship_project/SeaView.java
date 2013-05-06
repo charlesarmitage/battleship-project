e4ee0fb8ef57da483cc4ed34f47c439cfc5864cd
charlesarmitage <code@cjrarmitage.co.uk>
2013-05-06 22:49:56 +0100

    Add empty sea

--- src/main/java/com/example/battleship_project/SeaView.java
+++ src/main/java/com/example/battleship_project/SeaView.java
@@ -9,5 +9,6 @@ import android.os.Bundle;
 public class SeaView extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
     }
 }
