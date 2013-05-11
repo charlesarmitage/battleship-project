d040e0100edbfcc22261d66cebe8bff531295b24
Charles Armitage <code@cjrarmitage.co.uk>
2013-05-11 13:05:11 +0100

    Add debug touch event data and layout size data
    Add debug data for touch events and layout size to explore the api for
    touch events and view size.

--- src/main/java/com/cjra/battleship_project/FleetDeploymentActivity.java
+++ src/main/java/com/cjra/battleship_project/FleetDeploymentActivity.java
@@ -2,12 +2,18 @@ package com.cjra.battleship_project;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
- * Default FleetDeploymentActivity activity
+ * Default Battleship activity
  */
-public class FleetDeploymentActivity extends Activity implements FleetDeploymentView {
+public class FleetDeploymentActivity extends Activity
+        implements FleetDeploymentView,
+        View.OnTouchListener,
+        View.OnLayoutChangeListener {
 
     private OwnSeaGridController battleshipGame;
     private OwnSeaGridModel seaGrids;
@@ -21,6 +27,10 @@ public class FleetDeploymentActivity extends Activity implements FleetDeployment
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        ImageView seaGrid = (ImageView)findViewById(R.id.imageView);
+        seaGrid.addOnLayoutChangeListener(this);
+        seaGrid.setOnTouchListener(this);
     }
 
     @Override
@@ -41,5 +51,24 @@ public class FleetDeploymentActivity extends Activity implements FleetDeployment
 
     @Override
     public void displayFleetGrid() {
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        TextView debugText = (TextView)findViewById(R.id.debug_text);
+        debugText.setText("Touch event: " + event.getX() + ", " + event.getY());
+        return false;
+    }
+
+    @Override
+    public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+        displayDebugInfo();
+    }
+
+    private void displayDebugInfo() {
+        TextView debugText = (TextView)findViewById(R.id.debug_text);
+        ImageView seaGrid = (ImageView)findViewById(R.id.imageView);
+
+        debugText.setText("Grid: " + seaGrid.getBottom() + ", W: " + seaGrid.getWidth());
     }
 }
